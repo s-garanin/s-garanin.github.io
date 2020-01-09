3a66032eba8f72ed77ebc88d4a0232670b7bba41
Stanislav <56078359+s-garanin@users.noreply.github.com>
2020-01-10 00:58:03 +0300

    Initial commit
    Date:      Fri Jan 03 00:58:03 2020 +0300
    
    Create empty
    
    Create empty
    
    Create empty
    
    Add files via upload
    
    Add files via upload
    
    Delete empty
    
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    Initial commit

---
+++ js/js.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true
+    });
+});
+
+// Resent post slider
+$('.resent-post-slider').slick({
+    arrows: true
+});
+
+
+// Post image slider 
+$('.slider-post-image').slick({
+    dots: true,
+    arrows: false
+});
+// Calculation statistics
+var time = 2;
+cc = 1;
+$(window).scroll(function() {
+    $('#counter').each(function() {
+        var cPos = $(this).offset().top,
+            topWindow = $(window).scrollTop();
+        if (cPos < topWindow + 500) {
+            if (cc < 2) {
+                $(".number").addClass('viz');
+                $('h5').each(function() {
+                    var
+                        i = 1,
+                        num = $(this).data('num'),
+                        step = 100 * time / num,
+                        that = $(this),
+                        int = setInterval(function() {
+                            if (i <= num) {
+                                that.html(i);
+                            } else {
+                                cc = cc + 2;
+                                cc
+                                clearInterval(int);
+                            }
+                            i++;
+                        }, step);
+                });
+            }
+        }
+    });
+});
